9a5840440fa348fa11d95f689760910f4a259151
djrenren <djrenren@users.noreply.github.com>
2020-09-08 02:35:31 +0000

    deploy: c25707bce7e52a241164c41782f4221c26e390f0

--- precache-manifest.71ba06b0d9b543cc95d24c558819ca9c.js
+++ precache-manifest.71ba06b0d9b543cc95d24c558819ca9c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cdfa5e30cbb734ce6abb42975b467d5a",
+    "revision": "080b965f7b3dd1ba83084a1333b4d160",
     "url": "/battlegrid/index.html"
   },
   {
-    "revision": "46ed7ed541ca804c6295",
+    "revision": "a222664a047042b53b86",
     "url": "/battlegrid/static/css/main.9d34e759.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/battlegrid/static/js/2.ee7ae534.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "46ed7ed541ca804c6295",
-    "url": "/battlegrid/static/js/main.e5e838fa.chunk.js"
+    "revision": "a222664a047042b53b86",
+    "url": "/battlegrid/static/js/main.617a8b07.chunk.js"
   },
   {
     "revision": "82d2d981967f3d7e82e6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/battlegrid/precache-manifest.884cf03e551df162f1a03d657719f7e8.js"
+  "/battlegrid/precache-manifest.71ba06b0d9b543cc95d24c558819ca9c.js"
 );
 
 self.addEventListener('message', (event) => {
