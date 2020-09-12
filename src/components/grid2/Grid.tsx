1a556865a89caebb577528aacecc18f1f0f194e3
John Renner <john@jrenner.net>
2020-09-12 12:51:04 -0700

    Support file dropping

--- src/components/grid2/Grid.tsx
+++ src/components/grid2/Grid.tsx
@@ -15,6 +15,19 @@ export interface GridProps {
   dimensions: [number, number];
 }
 
+async function getDataURL(file: File): Promise<string> {
+  const reader = new FileReader();
+  return new Promise((resolve, reject) => {
+    reader.onload = () => resolve(reader.result as string);
+    reader.readAsDataURL(file);
+  })
+}
+
+function extractURLFromHTML(html: string): string | null {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  return doc.querySelector("img")?.src || null;
+}
 export function Grid(props: PropsWithChildren<GridProps>) {
   let dropLayer = useRef<HTMLDivElement>(null);
   let selection = useRef<any>(null);
@@ -33,14 +46,14 @@ export function Grid(props: PropsWithChildren<GridProps>) {
         hoverHint.current.style.top = Math.floor(coords[1] as number) + "em";
       }
     },
-    (ev) => {
+    async (ev) => {
       let coords = viewport.current!.clientToGrid([ev.clientX, ev.clientY]);
       let dataItems = ev.dataTransfer?.items ?? [];
-      let addItem = (s: string, i: number) => {
+      let addItem = (s: string) => {
         console.log(s);
         let item = {
           id: "" + Math.random(),
-          loc: [Math.floor(coords[0]) + i, Math.floor(coords[1])],
+          loc: [Math.floor(coords[0]), Math.floor(coords[1])],
           dim: [1,1],
           href: s,
         };
@@ -54,10 +67,21 @@ export function Grid(props: PropsWithChildren<GridProps>) {
         // } else {
         //   dataItems[i].getAsFile())
         // }
-        if (dataItems[i].kind.startsWith("image/")) {
-          addItem(window.URL.createObjectURL(dataItems[i].getAsFile()), 0);
-        } else if (dataItems[i].type === "text/uri-list") {
-          dataItems[i].getAsString((s) => addItem(s, 0));
+        console.log(dataItems[i].type)
+        if (dataItems[i].type.startsWith("image/")) {
+          const dataURL = await getDataURL(dataItems[i].getAsFile()!);
+          addItem(dataURL);
+          return;
+        } if (dataItems[i].type === "text/html") {
+          dataItems[i].getAsString(s => addItem(extractURLFromHTML(s)!));
+          return;
+        }
+        if (dataItems[i].type === "application/x-moz-file-promise-url") {
+          dataItems[i].getAsString(addItem)
+          return;
+        } else if (dataItems[i].kind === "string") {
+          let t = dataItems[i].type;
+          dataItems[i].getAsString(s => console.log(t, s));
         }
       }
     }
