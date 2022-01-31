cd72dfe2f09e809485c8d5c526b9271092e4fb5a
John Renner <john@jrenner.net>
2022-01-31 13:23:05 -0800

    Selection box UI

--- client/src/ui/canvas.ts
+++ client/src/ui/canvas.ts
@@ -3,9 +3,11 @@ import { customElement, property, state } from "lit/decorators.js";
 import { repeat } from "lit/directives/repeat.js";
 import { styleMap } from "lit/directives/style-map.js";
 import { mul_c } from "../util/math";
+import { stop_ev } from "../util/events";
 
-const GRID_SIZE = 24;
-const LINE_WIDTH = 0.5;
+const GRID_SIZE = 24; // scale-dependent px
+const LINE_WIDTH = 0.5; // scale-dependent px
+const HANDLE_SIZE = 5; // scale-independent px
 @customElement("bg-canvas")
 export class Canvas extends LitElement {
   @property({ type: Number })
@@ -14,20 +16,22 @@ export class Canvas extends LitElement {
   @property({ type: Number })
   height = 10;
 
-  tokens: TokenData[] = [
-    {
-      x: 48,
-      y: 48,
-      height: 48,
-      width: 48,
-      res: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==",
-      id: "1",
-    },
-  ];
+  tokens: Map<string, TokenData> = new Map([
+    [
+      "1",
+      {
+        x: 48,
+        y: 48,
+        height: 48,
+        width: 48,
+        res: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==",
+        id: "1",
+      },
+    ],
+  ]);
 
   constructor() {
     super();
-    this.addEventListener("click", this.#unfocus);
   }
 
   get #dim() {
@@ -37,6 +41,7 @@ export class Canvas extends LitElement {
   render() {
     let width = this.width * GRID_SIZE;
     let height = this.height * GRID_SIZE;
+    let selected = this.tokens.get(this.selection!);
     return html`
       <style>
         :host {
@@ -45,7 +50,7 @@ export class Canvas extends LitElement {
         }
       </style>
       <bg-viewport style="width: 100%; height: 100%">
-        <svg width=${width} height=${height}>
+        <svg width=${width} height=${height} @click=${this.#unfocus}>
           <defs>
             <pattern id="pat" x=${-LINE_WIDTH / 2} y=${-LINE_WIDTH / 2} width=${GRID_SIZE} height="100%" patternUnits="userSpaceOnUse">
               <rect class="gridline" x="0" y="0" width=${LINE_WIDTH} height="100%" fill="grey" opacity="1"></rect>
@@ -60,7 +65,7 @@ export class Canvas extends LitElement {
           <rect x="0" y="0" width=${width} height=${height} fill="url(#pat2)" rx="5" pointer-events="none"></rect>
 
           ${repeat(
-            this.tokens,
+            this.tokens.values(),
             (t) => t.id,
             (t, index) => svg`
                 <image
@@ -75,6 +80,39 @@ export class Canvas extends LitElement {
                 />
                 `
           )}
+          ${selected
+            ? svg`
+            <rect
+                class="selection"
+                x=${selected.x}
+                y=${selected.y}
+                width=${selected.width}
+                height=${selected.height}
+                fill="transparent"
+                @click=${stop_ev}
+            ></rect>
+            <line id="r-n" x1=${selected.x} y1=${selected.y} x2=${selected.x + selected.width} y2=${selected.height}></line>
+            <line id="r-w" x1=${selected.x} y1=${selected.y} x2=${selected.x} y2=${selected.y + selected.height}></line>
+            <line id="r-e" x1=${selected.x + selected.width} y1=${selected.y} x2=${selected.x + selected.width} y2=${
+                selected.y + selected.height
+              }></line>
+            <line id="r-s" x1=${selected.x} y1=${selected.y + selected.height} x2=${selected.x + selected.width} y2=${
+                selected.height + selected.height
+              }></line>
+            <rect id="r-nw" class="handle" x=${selected.x - HANDLE_SIZE / 2} y=${selected.y - HANDLE_SIZE / 2} width=${HANDLE_SIZE + "px"} height=${
+                HANDLE_SIZE + "px"
+              }></rect>
+            <rect id="r-ne" class="handle" x=${selected.x + selected.width - HANDLE_SIZE / 2} y=${
+                selected.y - HANDLE_SIZE / 2
+              } width=${HANDLE_SIZE} height=${HANDLE_SIZE}></rect>
+            <rect id="r-sw" class="handle" x=${selected.x - HANDLE_SIZE / 2} y=${
+                selected.y + selected.height - HANDLE_SIZE / 2
+              } width=${HANDLE_SIZE} height=${HANDLE_SIZE}></rect>
+            <rect id="r-se" class="handle" x=${selected.x + selected.width - HANDLE_SIZE / 2} y=${
+                selected.y + selected.height - HANDLE_SIZE / 2
+              } width=${HANDLE_SIZE} height=${HANDLE_SIZE}></rect>
+          `
+            : null}
         </svg>
       </bg-viewport>
     `;
@@ -104,6 +142,7 @@ export class Canvas extends LitElement {
       position: relative;
       display: inline-block;
       box-sizing: content-box !important;
+      --selection-color: cornflowerblue;
     }
 
     :host,
@@ -115,8 +154,49 @@ export class Canvas extends LitElement {
       position: absolute;
     }
 
-    .selected {
-      border: 3px solid blue;
+    .selection {
+      stroke: var(--selection-color);
+      stroke-width: 1;
+      vector-effect: non-scaling-stroke;
+      filter: drop-shadow(0px 0px 2px var(--selection-color));
+    }
+
+    .selection:active {
+      cursor: move;
+    }
+
+    .handle {
+      vector-effect: non-scaling-size;
+      fill: white;
+      stroke: var(--selection-color);
+    }
+
+    #r-n,
+    #r-s,
+    #r-e,
+    #r-w {
+      stroke-width: ${HANDLE_SIZE};
+      vector-effect: non-scaling-stroke;
+      stroke: transparent;
+    }
+
+    #r-ne,
+    #r-sw {
+      cursor: nesw-resize;
+    }
+    #r-nw,
+    #r-se {
+      cursor: nwse-resize;
+    }
+
+    #r-n,
+    #r-s {
+      cursor: row-resize;
+    }
+
+    #r-e,
+    #r-w {
+      cursor: col-resize;
     }
 
     bg-viewport::part(background) {
