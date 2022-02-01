34d60d7afc41e2440c97d95c8b3583696c44cf8a
John Renner <john@jrenner.net>
2022-01-31 16:31:28 -0800

    Set up github deploy

--- client/rollup.config.ts
+++ client/rollup.config.ts
@@ -4,6 +4,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import summary from "rollup-plugin-summary";
 import visualizer from "rollup-plugin-visualizer";
 import minifyHTML from "rollup-plugin-minify-html-literals";
+import copy from 'rollup-plugin-copy'
 
 let plugins = [
   typescript(),
@@ -20,6 +21,11 @@ let plugins = [
       toplevel: true,
     },
   }),
+  copy({
+    targets: [
+      {src: "static/**/*", dest: "build"}
+    ]
+  }),
   visualizer(),
   // Print bundle summary
   summary({}),
@@ -30,7 +36,6 @@ export default [
     plugins,
     input: "src/main.ts",
     output: {
-      sourcemap: true,
       file: "build/bundle.js",
       format: "es",
     },
